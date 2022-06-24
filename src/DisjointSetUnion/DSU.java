46d4eb05ec6efa4987f665525bc8707b07f0079e
ProfessorAtomicManiac <cyific@gmail.com>
2022-06-24 11:06:18 -0700

    Disjoint Set Union

--- src/DisjointSetUnion/DSU.java
+++ src/DisjointSetUnion/DSU.java
@@ -8,6 +8,17 @@ import java.util.StringTokenizer;
 
 public class DSU <T> {
 
+    // Can't avoid raw types
+    Node[] nodes;
+    public DSU(int n)
+    {
+        nodes = new Node[n];
+        for (int i = 0; i < n; i++)
+        {
+            nodes[i] = new Node<>(null, i);
+            nodes[i].parent = nodes[i];
+        }
+    }
     public void union(Node<T> node1, Node<T> node2)
     {
         Node<T> root1 = find(node1);
@@ -17,7 +28,7 @@ public class DSU <T> {
 
     public Node<T> find(Node<T> node)
     {
-        if (node.parent != null) {
+        if (node.parent != node) {
             node.parent = find(node.parent);
         }
         return node.parent;
@@ -37,7 +48,7 @@ public class DSU <T> {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        DSU<Integer> dsu = new DSU<>();
+        DSU<Integer> dsu = new DSU<>(n);
         for (int i = 0; i < m; i++)
         {
             st = new StringTokenizer(br.readLine());
